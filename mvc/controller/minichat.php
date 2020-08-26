10ad486aee3c469da535aede34f038765ac841b8
Pierre Berry <pierreberry@hotmail.fr>
2020-08-26 15:08:39 +0200

    Question minichat

--- mvc/controller/minichat.php
+++ mvc/controller/minichat.php
@@ -4,6 +4,7 @@ use OpenClassrooms\Blog\Model\MinichatManager;
 
 require_once(__DIR__ . '/../model/MinichatManager.php');
 
+// Est ce que je dois mettre ça ? 
 function miniChat()
 {
     $minichatManager = new MinichatManager();
